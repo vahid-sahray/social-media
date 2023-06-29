96a95495e5e0d270965ca85b776c5d060179db68
vahid <vahidsahray93@gmail.com>
2023-06-29 22:19:36 +0330

    create email backend

--- account/views.py
+++ account/views.py
@@ -7,6 +7,7 @@ from.forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class UserRegisterView(View):
@@ -59,7 +60,7 @@ class UserLoginView(View):
         return render(request, self.template_name, {'form': form})
 
 
-class UserLogoutView(View):
+class UserLogoutView(LoginRequiredMixin, View):
     def get(self, request):
         logout(request)
         messages.success(request, 'you logged out successfully', 'success')
