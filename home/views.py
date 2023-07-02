751e720c7d9499ff66f7fff2624a0462176174ff
vahid <vahidsahray93@gmail.com>
2023-07-02 15:36:37 +0330

    complated updated post

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from typing import Any
 from django import http
+from django.http.request import HttpRequest
 from django.http.response import HttpResponseBase
 from django.shortcuts import render, redirect
 from django.views import View
@@ -7,6 +8,7 @@ from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .forms import PostUpdateForm
+from django.utils.text import slugify
 
 
 class HomeView(View):
@@ -35,17 +37,30 @@ class PostDeleteView(LoginRequiredMixin, View):
 class PostUpdateView(LoginRequiredMixin, View):
     form_class = PostUpdateForm
 
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        return super().setup(request, *args, **kwargs)
+
     def dispatch(self, request, *args, **kwargs):
-        post = Post.objects.get(pk=kwargs['post_id'])
+        post = self.post_instance
         if not post.user.id == request.user.id:
             messages.error(request, 'you can not update this post', 'danger')
             return redirect('home:home')
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request, post_id):
-      post = Post.objects.get(pk=post_id)
+    def get(self, request, *args, **kwargs):
+      post = self.post_instance
       form = self.form_class(instance=post)
       return render(request, 'home/update.html', {'form': form})
 
-    def get(self, request, post_id):
-        pass
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.save()
+            messages.success(request, 'you updated this post', 'success')
+            return redirect('home:post_detail', post.id, post.slug)
+        
+
