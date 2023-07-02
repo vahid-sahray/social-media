b6730ab89dbee0338979417e16cb87614c115e52
vahid <vahidsahray93@gmail.com>
2023-07-02 18:46:40 +0330

    page not found Error404 handeling

--- home/views.py
+++ home/views.py
@@ -1,8 +1,4 @@
-from typing import Any
-from django import http
-from django.http.request import HttpRequest
-from django.http.response import HttpResponseBase
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -19,13 +15,13 @@ class HomeView(View):
 
 class PostDetailView(View):
     def get(self, request, post_id, post_slug):
-        post = Post.objects.get(pk=post_id, slug=post_slug)
+        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
         return render(request, 'home/detail.html', {'post': post})
     
 
 class PostDeleteView(LoginRequiredMixin, View):
     def get(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+        post = get_object_or_404(Post, pk=post_id)
         if post.user.id == request.user.id:
             post.delete()
             messages.success(request, 'post deleted successfully', 'success')
@@ -38,7 +34,7 @@ class PostUpdateView(LoginRequiredMixin, View):
     form_class = PostCreateUpdateForm
 
     def setup(self, request, *args, **kwargs):
-        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'])
         return super().setup(request, *args, **kwargs)
 
     def dispatch(self, request, *args, **kwargs):
--- social/settings.py
+++ social/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9e+m(8g)+ma*6#xk4lg*-#*bs2u@ys)pifh-ykh%@^s+4z$!wd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
