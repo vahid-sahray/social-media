70a5a5581be8b1e457aaa5ee1a4dc532100d118b
vahid <vahidsahray93@gmail.com>
2023-07-02 06:28:29 +0330

    create detail pag for show posts

--- home/admin.py
+++ home/admin.py
@@ -6,6 +6,6 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('user', 'slug', 'updated')
     search_fields = ('slug',)
-    list_filter  = ('updated')
+    list_filter  = ('updated',)
     prepopulated_fields = {'slug': ('body',)}
     raw_id_fields = ('user',)
