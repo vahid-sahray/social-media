3c9dcfa1ebbd0b736dd68531ce29e1ea69891a16
vahid <vahidsahray93@gmail.com>
2023-07-03 19:07:43 +0330

    strted gmail smtp

--- social/settings.py
+++ social/settings.py
@@ -130,3 +130,14 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authenticate.EmailBackend',
 ]
+
+
+# Google account
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'vahidsahray93@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'pxufrtwikthlbsxe'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Vahid-s'
